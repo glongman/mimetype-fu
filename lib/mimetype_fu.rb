ca229705287ee02f21bd601506430d19fac9bdab
Vincent Hellot <vincent@hellmac.(none)>
2008-04-23 02:05:32 +0200

    use ruby-filemagic on unixes => avoid system calls (file) and incompatible option for file

--- lib/mimetype_fu.rb
+++ lib/mimetype_fu.rb
@@ -1,35 +1,40 @@
 class File
-
+  # resulting type for an uploaded file is somewhat tricky
+  # it depends mostly of uploaded file size
+  # see actioncontroller request.rb file for more details
   def self.mime_type?(file)
-     if file.class == File
-       unless RUBY_PLATFORM.include? 'mswin32'
-         mime = `file -bir #{file.path}`.strip
-       else
-         mime = EXTENSIONS[File.extname(file.path).gsub('.','').downcase.to_sym]
-       end
-     elsif file.class == String
-       mime = EXTENSIONS[(file[file.rindex('.')+1, file.size]).downcase.to_sym]
-     elsif file.class == StringIO
-       temp = File.open(Dir.tmpdir + '/upload_file.' + Process.pid.to_s, "wb")
-       temp << file.string
-       temp.close
-       mime = `file -bir #{temp.path}`
-       mime = mime.gsub(/^.*: */,"")
-       mime = mime.gsub(/;.*$/,"")
-       mime = mime.gsub(/,.*$/,"")
-       File.delete(temp.path)
-     end
+    if RUBY_PLATFORM.include? 'mswin32'
+      case file
+      when File
+        EXTENSIONS[File.extname(file.path).delete('.').downcase.to_sym]
+      when String
+        EXTENSIONS[File.extname(file).delete('.').downcase.to_sym]
+      else
+        "unknown/unknown" 
+      end
+    else
+      require 'filemagic'
+      fm = FileMagic.new(FileMagic::MAGIC_MIME)
+      case file
+      when File
+        fm.file(file.path)
+      when String
+        fm.file(file)
+      when StringIO
+        fm.buffer(file.string)
+      when ActionController::UploadedStringIO
+        fm.buffer(file.string)
+      when ActionController::UploadedTempfile
+        file.rewind
+        fm.buffer(file.read(10))
+      else
+        "unknown/unknown" 
+      end
+    end
+  end
 
-     if mime
-       return mime
-     else
-       'unknown/unknown'
-     end
-   end
-
-  
   def self.extensions
     EXTENSIONS
   end
-  
 end
+    
